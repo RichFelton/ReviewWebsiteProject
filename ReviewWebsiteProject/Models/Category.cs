431a953a1f08fb041bece25d6d7877bed3dd6819
RichFelton <rfelton.math@gmail.com>
2018-07-16 04:32:58 -0400

    first commit

---
+++ ReviewWebsiteProject/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReviewWebsiteProject.Models
+{
+    public class Category
+    {
+        public int ID { get; set; }
+        public string PhoneMaker { get; set; }
+        public string PhoneModel { get; set; }
+        public double PhonePrice { get; set; }
+        public int MemorySize { get; set; }
+        public double ScreenSize { get; set; }
+        public string OperatingSystem { get; set; }
+
+        public virtual ICollection<Review> Reviews { get; set; }
+    }
+}
---
+++ ReviewWebsiteProject/Models/Review.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ReviewWebsiteProject.Models
+{
+    public class Review
+    {
+        [Key]
+        public int ReviewID { get; set; }
+        public string Author { get; set; }
+        public string WherePublished { get; set; }
+        public DateTime PublishDate { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
